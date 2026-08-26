79c1565ec1542fe3a38aea6990d9ae7312919472
Nat-24 <vnat615@gmail.com>
2026-08-26 10:09:46 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+
+import tailwindcss from "@tailwindcss/vite";
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/style.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+})
+
+
+
+
+
